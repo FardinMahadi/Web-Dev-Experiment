1b48176473809099cd97319f31954c32b11a77fc
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-06-19 22:49:37 +0600

    add

---
+++ JavaScript/Numbers.js
@@ -0,0 +1,22 @@
+console.log(typeof Infinity);
+console.log(typeof NaN);
+console.log(0b100);
+
+let x = 42; // Integer
+let y = 3.14; // Floating-point
+let z = 1e3; // Exponential notation (1000)
+let bin = 0b1010; // Binary (10)
+let hex = 0xff; // Hexadecimal (255)
+let oct = 0o77; // Octal (63)
+
+console.log(x, y, z, bin, hex, oct);
+console.log(parseInt("100"));
+
+console.log(typeof ("abc" * 2));
+console.log(2 ** 4);
+
+console.log(0.1 + 0.2 === 0.3);
+
+const name = "fardin";
+// name = { name: "Hasan" };
+console.log(name);
